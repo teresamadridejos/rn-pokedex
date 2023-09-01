b7ff369df405f13040aa9b23fbf6f9d44f96c6ad
Teresa <teresamadridejosm@gmail.com>
2023-09-01 11:06:24 +0200

    Pokemons card styled

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,15 +1,15 @@
-import { Text, View, StyleSheet, Image } from 'react-native';
-import useFetch from '../hooks/useFetch';
+import React from "react";
+import { Text, View, StyleSheet, Image, ScrollView } from "react-native";
+import useFetch from "../hooks/useFetch";
 
 interface PokemonCardProps {
   url: string;
 }
 
-
-
 export function PokemonCard({ url }: PokemonCardProps) {
-  const {data, loading, error } = useFetch(url);
+  const { data, loading, error } = useFetch(url);
   console.log(data);
+  const { color } = useFetch('https://pokeapi.co/api/v2/pokemon-color/');
 
   if (loading) {
     return <Text>Loading...</Text>;
@@ -20,31 +20,55 @@ export function PokemonCard({ url }: PokemonCardProps) {
   }
 
   return (
-    <View style={styles.container}>
-      <Image
-        source={{
-          uri: data.sprites.other['official-artwork'].front_default,
-        }}
-        style={styles.image}
-      />
-      <Text style={styles.name}>{data.name}</Text>
+    <View style={styles.card}>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>{data.name}</Text>
+        <Text style={styles.text}>#{data.id}</Text>
+      </View>
+      <View style={styles.imageContainer}>
+        <Image
+          source={{
+            uri: data.sprites.other["official-artwork"].front_default,
+          }}
+          style={styles.image}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  card: {
     padding: 8,
-    flexDirection: 'row',
-    alignItems: 'center',
+    alignItems: "flex-start",
+    width: 130,
+    height: 90,
+    backgroundColor: "white",
+    borderRadius: 8,
+    elevation: 5,
+    shadowColor: "rgba(0, 0, 0, 0.5)",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 1,
+    shadowRadius: 4,
+    margin: 8,
   },
-  image: {
+  imageContainer: {
+    position: "absolute",
+    right: -30,
+    top: 25,
     width: 100,
     height: 100,
+  },
+  image: {
+    width: "80%",
+    height: "80%",
+  },
+  textContainer: {
+    flexDirection: "column",
     marginRight: 32,
   },
-  name: {
-    fontWeight: 'bold',
-    fontSize: 32,
+  text: {
+    fontWeight: "bold",
+    fontSize: 16,
   },
 });
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,44 +1,35 @@
-import {
-  StyleSheet,
-  Text,
-  SafeAreaView,
-  FlatList,
-  View,
-} from 'react-native';
-import { useContext } from 'react';
-import { AppContext } from '../context/Context';
-import { PokemonCard } from '../components/PokemonCard';
-import { Pokemon } from '../interfaces/Pokemon.interface';
-
+import React from "react";
+import { StyleSheet, Text, SafeAreaView, FlatList, View } from "react-native";
+import { useContext } from "react";
+import { AppContext } from "../context/Context";
+import { PokemonCard } from "../components/PokemonCard";
+import { Pokemon } from "../interfaces/Pokemon.interface";
 
 export default function HomeScreen() {
-  const { state } = useContext(AppContext); // Obtén el estado de tu contexto global
-
-  // const { pokemons } = state; // Extrae el arry de Pokémon del estado
-  console.log(state.pokemons);
-  // console.log(pokemons[1].url);
-  
-
+  const { state } = useContext(AppContext);
 
   return (
-    <SafeAreaView >
-    {state.pokemons.length === 0 ? (
-      <Text>Cargando...</Text>
-    ) : (
-
-      // state.pokemons[0].map((item)=> item.name)
-      <FlatList
-        data={state.pokemons[0]}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => (
-          
-          <PokemonCard url={item.url} />
-            // <Text>{item.url}</Text>
-          
-        )}
-      />
-    )}
-  </SafeAreaView>
+    <SafeAreaView>
+      <View >
+        <Text style={styles.title}>Pokedex</Text>
+      </View>
+      {state.pokemons.length === 0 ? (
+        <Text>Cargando...</Text>
+      ) : (
+        <FlatList
+          data={state.pokemons[0]}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => <PokemonCard url={item.url} />}
+        />
+      )}
+    </SafeAreaView>
   );
 }
 
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    paddingHorizontal: 16,
+  },
+});
